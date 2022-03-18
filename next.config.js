04c507fce7448a98bcf51b502400c3453aed9a5d
DeiAsPie <deiaspie@deiaspie.deiaspie>
2022-03-18 07:40:01 +0000

    fixing workflow attempt 6

--- next.config.js
+++ next.config.js
@@ -8,5 +8,9 @@ module.exports = nextConfig
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
+  images: {
+    loader: 'custom',
+    path: 'https://deiaspie.github.io/courses',
+  },
   assetPrefix: isProd ? '/courses' : ''
 }
