5100f93548d038fc901845343d2a313bd6c1e99c
DeiAsPie <93835541+DeiAsPie@users.noreply.github.com>
2022-08-14 18:38:14 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,9 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 }
 
 module.exports = nextConfig
 
-const isProd = process.env.NODE_ENV === 'production';
-
 module.exports = {
-  images: {
-    loader: 'imgix',
-  }
-  basePath: '/courses',
+  assetPrefix: '/courses',
 };
