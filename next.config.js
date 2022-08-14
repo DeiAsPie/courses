1052732d130d6d1440412d50f53b6fb6771ca7a9
DeiAsPie <deiaspie@deiaspie.deiaspie>
2022-08-14 07:39:00 +0000

    Revert "Delete next.config.js"
    This reverts commit 8be8ef9cc132064e52b2dc891062add0be56bffb.

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
+
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  images: {
+    loader: 'imgix',
+    path: 'https://deiaspie.github.io/',
+  },
+  assetPrefix: isProd ? '/courses' : '',
+};
