f55929a3e35e7fa8ed7c85fcd9c1af2de6ff11a1
DeiAsPie <93835541+DeiAsPie@users.noreply.github.com>
2022-10-26 16:16:15 +0000

    attempt 13

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
         <footer className="bottom center">
             <small>
-                <p>Copyright © 2021-2022 <strong>DeiAsPie</strong>. This work by <strong>DeiAsPie</strong> is licensed under <a href="http://creativecommons.org/licenses/by-nc-nd/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-nd/4.0/80x15.png" /></a></p>
+                <p>Copyright © 2021-2022 <strong>DeiAsPie</strong>. This work by <strong>DeiAsPie</strong> is licensed under <a href="http://creativecommons.org/licenses/by-nc-nd/4.0/" target="_blank"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-nd/4.0/80x15.png" /></a></p>
             </small>
         </footer>
   )
