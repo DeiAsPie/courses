0e3615a0036845ade1c1a2eb45e3a8ea8f7c542b
DeiAsPie <93835541+DeiAsPie@users.noreply.github.com>
2022-10-28 10:50:58 +0000

    add links

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,35 @@ export default function Header() {
   return (
       <header className='top center sticker glassy'>
           <div >
-            <h1>DeiAsPie's Recommended Courses 🙂</h1>
+            <h1>
+              DeiAsPie's Recommended Courses 🙂
+            </h1>
+            
+            <section class="tabs">
+              <a href="https://open.lbry.com/@DeiAsPieBlog:f">
+                <section class="center button">
+                    <strong>Tech Blog 📜 →</strong>
+                </section>
+              </a>
+
+              <a href="https://open.lbry.com/@DeiAsPie:1">
+                <section class="center button">
+                    <strong>Tech Videos 🎬 →</strong>
+                </section>
+              </a>
+
+              <a href="https://open.lbry.com/@DeiAsPieMindSpace:7">
+                <section class="center button">
+                    <strong>Mind Space 🧠 →</strong>
+                </section>
+              </a>
+
+              <a href="https://github.com/DeiAsPie">
+                <section class="center button">
+                    <strong>GitHub 🐙 →</strong>
+                </section>
+              </a>
+            </section>
           </div>
       </header>
   )
