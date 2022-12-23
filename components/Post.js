b9b6be614f5eb26ff4db02adf315e496a991b17f
DeiAsPie <Github@DeiAsPie.slmail.me>
2022-12-23 12:43:21 +0530

    lazy loading images attempt 0

--- components/Post.js
+++ components/Post.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Post({ post }) {
   return (
     <div className='card'>
-      <img src={post.frontmatter.cover_image} alt='' />
+      <img src={post.frontmatter.cover_image} alt='course image' loading="lazy"/>
 
       <h3>{post.frontmatter.title}</h3>
 
