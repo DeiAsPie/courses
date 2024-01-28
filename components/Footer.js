503c0dee32377770d2ff16a2a1cd80ed53b7dbd0
DeiAsPie <93835541+DeiAsPie@users.noreply.github.com>
2024-01-28 19:27:03 +0530

    well! its new year

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
         <footer className="bottom center">
             <small>
-                <p>Copyright © 2023 <strong>DeiAsPie</strong>. This work by <strong>DeiAsPie</strong> is licensed under <a href="http://creativecommons.org/licenses/by-nc-nd/4.0/" target="_blank"><img src="https://i.creativecommons.org/l/by-nc-nd/4.0/80x15.png" alt="Creative Commons License" class="small-icon"/></a></p>
+                <p>Copyright © 2024 <strong>DeiAsPie</strong>. This work by <strong>DeiAsPie</strong> is licensed under <a href="http://creativecommons.org/licenses/by-nc-nd/4.0/" target="_blank"><img src="https://i.creativecommons.org/l/by-nc-nd/4.0/80x15.png" alt="Creative Commons License" class="small-icon"/></a></p>
             </small>
         </footer>
   )
